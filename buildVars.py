0a8afd7feecb82c323f489bcdf37c45e1ad42934
cary-rowen <manchen_0528@outlook.com>
2023-05-21 22:57:28 +0800

    Release 1.6.6

--- buildVars.py
+++ buildVars.py
@@ -25,21 +25,27 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""增强 PC微信的使用体验，修复因PC微信无障碍缺陷导致的多处列表项朗读问题，支持朗读公众号留言，修正错误的按钮标签，添加快速键等。"""),
 	# version
-	"addon_version": "1.6.5",
+	"addon_version": "1.6.6",
 	# Author(s)
-	"addon_author": "沉浮, Eureka",
+	"addon_author": "沉浮, Cary-Rowen <manchen_0528@outlook.com>",
 	# URL for the add-on documentation support
-	"addon_url": None,
+	"addon_url": "https://www.nvdacn.com/",
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": "https://github.com/cary-rowen/WeChatEnhancement",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": 2021.2,
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": 2022.2,
+	"addon_lastTestedNVDAVersion": 2023.1,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": "GPL v2",
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
 
 # Define the python files that are the sources of your add-on.
