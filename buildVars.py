e3b7736c22cb6ec9ee5c6f003f604cd2fed007e6
cary-rowen <manchen_0528@outlook.com>
2026-05-01 17:16:45 +0800

    Bump version, compatible with NVDA 2026.1.0

--- buildVars.py
+++ buildVars.py
@@ -23,10 +23,12 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""Enhance the experience of using PC WeChat for NVDA users."""),
 	# version
-	addon_version="1.8.8",
+	addon_version="2.0",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""Update the add-on build template."""),
+	addon_changelog=_("""### 2.0
+
+Bump version, compatible with NVDA 2026.1.0."""),
 	# Author(s)
 	addon_author="Cary-Rowen <manchen_0528@outlook.com>",
 	# URL for the add-on documentation support
@@ -38,7 +40,7 @@ addon_info = AddonInfo(
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2022.1.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.1.0",
+	addon_lastTestedNVDAVersion="2026.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
