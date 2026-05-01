7ebb22a35cc4a8c4297753a5a2d77135ab4c0b19
cary-rowen <manchen_0528@outlook.com>
2026-05-01 17:31:17 +0800

    Bump version to 2.0.1

--- buildVars.py
+++ buildVars.py
@@ -23,12 +23,12 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""Enhance the experience of using PC WeChat for NVDA users."""),
 	# version
-	addon_version="2.0",
+	addon_version="2.0.1",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""### 2.0
+	addon_changelog=_("""### 2.0.1
 
-Bump version, compatible with NVDA 2026.1.0."""),
+Add missing popup sound."""),
 	# Author(s)
 	addon_author="Cary-Rowen <manchen_0528@outlook.com>",
 	# URL for the add-on documentation support
--- changelog.md
+++ changelog.md
@@ -1,3 +1,3 @@
-### 2.0
+### 2.0.1
 
-Bump version, compatible with NVDA 2026.1.0.
+Add missing popup sound.
